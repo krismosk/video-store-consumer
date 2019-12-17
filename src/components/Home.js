ffd9928745c728cdb997d524adb197dfab69e4a7
Kristina <kristina.tanya@gmail.com>
2019-12-17 15:44:57 -0800

    cleaned up comments, rentals working. conditional rendering needs to be added

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,7 +11,7 @@ class Home extends React.Component {
 
     this.state = {
       selectedCustomer: '',
-      selectedMovie: ''
+      selectedMovie: '',
     };
   }
     
--- src/components/Rental.js
+++ src/components/Rental.js
@@ -5,7 +5,6 @@ let date = new Date();
 date.setDate(new Date().getDate() + 7);
 
  // make rental conditionally render if it has at least one customer or one movie
-  // rental component will make a post request to backend API
 
 class Rental extends React.Component {
   constructor(props) {
@@ -23,7 +22,7 @@ class Rental extends React.Component {
       movie_id: movie.id,
       due_date: dueDate.toISOString(),
     }
-    
+
     axios.post(`http://localhost:3000/rentals/${movie.title}/check-out`, params)
     .then((response) => {
       this.setState({rental: response.data})
