a828962b3b1eed3ca5af1acb414f3c32852541db
KKennedyCodes <holmstedtk@gmail.com>
2019-12-17 10:40:22 -0800

    merging with home branch

---
+++ src/components/Search.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './Search.css';
+
+const SearchBar = ({ searchTerm, searchChangeCallback }) => {
+  return (
+    <section>
+      <div>
+        <label className="search-bar--label" htmlFor="searchBar">Search</label>
+      </div>
+      <input
+        onChange={(event) => { searchChangeCallback(event.target.value) }}
+        value={searchTerm}
+        name="searchBar"
+        id="searchBar"
+        className="search-bar"
+      />
+    </section>
+  );
+};
+
+SearchBar.propTypes = {
+  searchChangeCallback: PropTypes.func.isRequired,
+  searchTerm: PropTypes.string.isRequired,
+};
+
+export default SearchBar;
