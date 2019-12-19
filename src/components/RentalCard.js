be63e0ac82fd78c34ff8ab78fecd48fc1ebb9449
Kristina <kristina.tanya@gmail.com>
2019-12-19 09:18:16 -0800

    added bootstrap card and buttons to rentallist componenet

--- src/components/RentalCard.js
+++ src/components/RentalCard.js
@@ -4,14 +4,16 @@ import './RentalCard.css';
 const RentalCard = ({ rentalId, movieTitle, customerId, customerName, checkoutDate, dueDate, checkinRental }) => {
   
   return (
-  <div>
-    <section className="RentalCard">
-      <p>Title: { movieTitle }</p>
-      <p>Customer: { customerName }</p>
-      <p>Checkout date: { checkoutDate } </p>
-      <p>Due date: { dueDate }</p>
-    </section>
-    <button onClick={() => {checkinRental(customerId, movieTitle, rentalId)}} type="button">Check in Rental</button>
+  <div className="card-deck rental-card">
+    <div className="card">
+      <div className="card-body">
+        <p>Title: { movieTitle }</p>
+        <p>Customer: { customerName }</p>
+        <p>Checkout date: { checkoutDate } </p>
+        <p>Due date: { dueDate }</p>
+        <button className="btn btn-primary" size="sm" onClick={() => {checkinRental(customerId, movieTitle, rentalId)}} type="button">Check in Rental</button>       
+      </div>
+    </div>
   </div>
   )
 }
